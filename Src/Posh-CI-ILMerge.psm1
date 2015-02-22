47b09ddc358cbf57f0c35653fa0693e38560cf8a
Chris Dostert <chris.dostert@live.com>
2015-02-21 17:28:27 -0800

    temp fix for "External packages cannot depend on packages that target projects." due to ilmerge dependency

--- Src/Posh-CI-ILMerge.psm1
+++ Src/Posh-CI-ILMerge.psm1
@@ -5,13 +5,17 @@ function Invoke-CIStep(
 [string][Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]$PoshCIProjectRootDirPath,
 [string[]][Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]$IlMergeParameters){
     
-        # invoke ilmerge.exe
-        & ilmerge $IlMergeParameters
+	# note: this should be in .nuspec but with current version of ilmerge package "2.12.0803" 
+	# receive error "External packages cannot depend on packages that target projects."
+	choco install ilmerge
+	
+    # invoke ilmerge.exe
+    & ilmerge $IlMergeParameters
 
-        # handle errors
-        if ($LastExitCode -ne 0) {
-            throw $Error
-        }
+    # handle errors
+    if ($LastExitCode -ne 0) {
+        throw $Error
+    }
 }
 
 Export-ModuleMember -Function Invoke-CIStep
